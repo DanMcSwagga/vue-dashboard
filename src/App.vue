f818858a03dca5496384de374496895f9c61a9ac
Dan Stetsenko <denyast@ukr.net>
2019-06-11 01:42:09 +0300

    Pagination & Searching completed.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,55 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="app-header">
+      <ShowItems />
+      <SearchItems />
     </div>
-    <router-view />
+
+    <TableData />
+
+    <div class="app-footer">
+      <!-- <Info /> -->
+      <Pagination />
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import TableData from '@/components/Table.vue'
+import ShowItems from '@/components/ShowItems.vue'
+import SearchItems from '@/components/SearchItems.vue'
+import Pagination from '@/components/Pagination.vue'
+// import Info from "@/components/Info.vue";
+
+export default {
+  name: 'app',
+
+  components: {
+    TableData,
+    ShowItems,
+    SearchItems,
+    Pagination
+    // Info,
+    // Upload
   }
+}
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  color: #73879c;
+  font-size: 14px;
+}
+
+.app-header,
+.app-footer {
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 2px;
+}
+
+.app-upload {
+  padding: 10px 0;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,46 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import data from './data'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    data,
+    curPage: 1,
+    perPage: 5,
+    searchText: ''
+  },
+
+  getters: {
+    fields: state => Object.keys(state.data[0]),
+
+    // TODO: rewrite/optimize
+    filteredData: state => {
+      return state.data.filter(
+        e =>
+          Object.values(e)
+            // .slice(1)
+            .join('~')
+            .toLowerCase()
+            .indexOf(state.searchText.toLowerCase()) > -1
+      )
+    },
+
+    rows: (state, getters) => {
+      return getters.filteredData.slice(
+        (state.curPage - 1) * state.perPage,
+        state.curPage * state.perPage
+      )
+    },
+
+    numPages: (state, getters) =>
+      Math.ceil(getters.filteredData.length / state.perPage)
+  },
+
+  mutations: {
+    set: (state, { key, value }) => (state[key] = value) // кей
+  },
+
   actions: {}
-});
+})
